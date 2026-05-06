778f4265b292dff0be6c863fc3b215c46481e17c
Troya <cristiancalvatroya@gmail.com>
2026-05-07 01:57:50 +0200

    mejoras

--- script.js
+++ script.js
@@ -56,7 +56,7 @@
         revealObserver.unobserve(entry.target);
       }
     });
-  }, { threshold: 0.12, rootMargin: '0px 0px -40px 0px' });
+  }, { threshold: 0.05, rootMargin: '0px 0px 0px 0px' });
 
   function applyReveals(){
     document.querySelectorAll('.reveal, .stagger').forEach(el => {
